5615e0ebd4304420dd4b0df884accf6f3d65b1a7
Paweł Kukliński <paffcio314@gmail.com>
2021-11-28 19:35:58 +0100

    rm cart and add some logic

--- src/booking/domain/model.py
+++ src/booking/domain/model.py
@@ -7,6 +7,10 @@ class SeatTaken(Exception):
     pass
 
 
+class SeatsCollide(Exception):
+    pass
+
+
 @dataclass(frozen=True)
 class Seat:
     row: str
@@ -17,18 +21,6 @@ class Theatre:
     def __init__(self, id: UUID, seats: Iterable[Seat]):
         self._id = id
         self._seats = set(seats)
-
-
-class Cart:
-    def __init__(self, seats: Iterable[Seat], customer_id: UUID):
-        self._seats = set(seats)
-        self.customer_id = customer_id
-
-    def add(self, seat: Seat):
-        if True:  # seat is available
-            self._seats.add(seat)
-        else:
-            raise SeatTaken("Choose other seat")
 
     @property
     def seats(self):
@@ -45,16 +37,54 @@ class Reservation:
         self.customer_id = customer_id
         self._seats = set(seats)
 
-    # this should be a domain layer function (check nomenclature)
-    @classmethod
-    def from_cart(cls, cart: Cart):
-        return cls(cart.seats, cart.customer_id)
+    def add(self, seat: Seat):
+        if seat not in self._seats:  # seat is available
+            self._seats.add(seat)
+        else:
+            raise SeatTaken("This seat has already been chosen")
+
+    @property
+    def seats(self):
+        return self._seats
+
+    def __hash__(self) -> int:
+        return hash(self.reservation_number)
 
 
 class Screening:
     def __init__(
-        self, reservations: Iterable[Reservation], theatre: Theatre, movie_id: UUID
+        self,
+        reservations: Iterable[Reservation],
+        theatre: Theatre,
+        movie_id: UUID,
+        screening_id: UUID,
     ):
+        self.screening_id = screening_id
         self._reservations = set(reservations)
         self.theatre = theatre
         self.movie_id = movie_id
+
+    def make(self, reservation: Reservation):
+        if self._seats_collide(reservation):
+            raise SeatsCollide("Some seats are already reserved")
+        self._reservations.add(reservation)
+
+    def _seats_collide(self, reservation: Reservation) -> bool:
+        if reservation in self._reservations:
+            return False
+        else:
+            return bool(reservation.seats & self._seats_taken)
+
+    @property
+    def _free_seats(self) -> set[Seat]:
+        return self.theatre.seats - self._seats_taken
+
+    @property
+    def _seats_taken(self) -> set[Seat]:
+        seats_taken = set()
+        for reservation in self._reservations:
+            seats_taken.union(reservation.seats)
+        return seats_taken
+
+    def __hash__(self) -> int:
+        return hash(self.screening_id)
