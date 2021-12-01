864de2d8392128f3c8669367da2592c4baee8556
Paweł Kukliński <paffcio314@gmail.com>
2021-12-01 22:56:26 +0100

    orm step 2

--- src/booking/adapters/orm.py
+++ src/booking/adapters/orm.py
@@ -15,12 +15,48 @@ screenings = Table(
 )
 
 movies = Table(
+    "movies",
     metadata,
     Column("movie_id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
     Column("title", String(255)),
 )
 
 reservations = Table(
+    "reservations",
     metadata,
+    Column("reservation_id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
     Column("screening_id", ForeignKey("screenings.screening_id")),
+    Column("customer_id", UUID(as_uuid=True), nullable=False),
+)
+
+reservations_seats = Table(
+    "reservations_seats",
+    metadata,
+    Column(
+        "reservation_id", ForeignKey("reservations.reservation_id"), primary_key=True
+    ),
+    Column("row", ForeignKey("seats.row"), primary_key=True),
+    Column("place", ForeignKey("seats.place"), primary_key=True),
+)
+
+theatres_seats = Table(
+    "theatres_seats",
+    metadata,
+    Column("theatre_id", ForeignKey("theatres.theatre_id"), primary_key=True),
+    Column("row", ForeignKey("seats.row"), primary_key=True),
+    Column("place", ForeignKey("seats.place"), primary_key=True),
+)
+
+theatres = Table(
+    "theatres",
+    metadata,
+    Column("theatre_id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
+    Column("name", String(255)),
+)
+
+seats = Table(
+    "seats",
+    metadata,
+    Column("row", String(3), nullable=False),
+    Column("place", Integer, nullable=False),
 )
