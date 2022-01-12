b13d6c01aa80e0f97b2220b2e38bcd53bf61e5d1
Paweł Kukliński <paffcio314@gmail.com>
2022-01-12 22:11:10 +0100

    update unit of work

--- src/booking/service_layer/unit_of_work.py
+++ src/booking/service_layer/unit_of_work.py
@@ -2,12 +2,8 @@
 from __future__ import annotations
 import abc
 from booking.adapters import repository
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from booking import config
 from django.db import transaction
+
 
 class AbstractUnitOfWork(abc.ABC):
     screenings: repository.AbstractRepository
@@ -27,38 +23,9 @@ class AbstractUnitOfWork(abc.ABC):
         raise NotImplementedError
 
 
-DEFAULT_SESSION_FACTORY = sessionmaker(
-    bind=create_engine(
-        config.get_postgres_uri(),
-        isolation_level="REPEATABLE READ",
-    )
-)
-
-
-class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
-    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
-        self.session_factory = session_factory
-
-    def __enter__(self):
-        self.session = self.session_factory()  # type: Session
-        self.screenings = repository.SqlAlchemyRepository(self.session)
-        return super().__enter__()
-
-    def __exit__(self, *args):
-        super().__exit__(*args)
-        self.session.close()
-
-    def commit(self):
-        self.session.commit()
-
-    def rollback(self):
-        self.session.rollback()
-
-
-
 class DjangoUnitOfWork(AbstractUnitOfWork):
     def __enter__(self):
-        self.batches = repository.DjangoRepository()
+        self.screenings = repository.DjangoRepository()
         transaction.set_autocommit(False)
         return super().__enter__()
 
@@ -67,8 +34,8 @@ class DjangoUnitOfWork(AbstractUnitOfWork):
         transaction.set_autocommit(True)
 
     def commit(self):
-        for batch in self.batches.seen:
-            self.batches.update(batch)
+        for screening in self.screenings.seen:
+            self.screenings.update(screening)
         transaction.commit()
 
     def rollback(self):
