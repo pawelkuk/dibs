f2c9a2ea57c6e67556e8440088c02dc81e456bc4
Paweł Kukliński <paffcio314@gmail.com>
2022-07-23 15:30:56 +0200

    wip paying service

---
+++ src/api/paying/adapters.py/repository.py
@@ -0,0 +1,41 @@
+import abc
+from uuid import UUID
+from paying.domain import model
+from paying import models as django_models
+
+
+class AbstractRepository(abc.ABC):
+    def __init__(self):
+        self.seen: set[model.Payment] = set()
+
+    def add(self, payment: model.Payment):
+        self.seen.add(payment)
+
+    def get(self, payment_id: UUID) -> model.Payment:
+        p = self._get(payment_id)
+        if p:
+            self.seen.add(p)
+        return p
+
+    @abc.abstractmethod
+    def _get(self, payment_id: UUID) -> model.Payment:
+        raise NotImplementedError
+
+
+class DjangoRepository(AbstractRepository):
+    def add(self, payment: model.Payment):
+        super().add(payment)
+        self.update(payment)
+
+    def update(self, payment: model.Payment):
+        django_models.Payment.update_from_domain(payment)
+
+    def _get(self, payment_id: UUID) -> model.Payment:
+        return (
+            django_models.Payment.objects.filter(payment_id=payment_id)
+            .first()
+            .to_domain()
+        )
+
+    def list(self) -> list[model.Payment]:
+        return [p.to_domain() for p in django_models.Payment.objects.all()]
