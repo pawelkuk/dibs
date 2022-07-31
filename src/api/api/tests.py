ff3d11026c15bd0de9490927511e8591ff58e1c7
Paweł Kukliński <paffcio314@gmail.com>
2022-07-31 14:02:29 +0200

    test argument passing

--- src/api/api/tests.py
+++ src/api/api/tests.py
@@ -7,6 +7,7 @@ from api.tasks import PaymentStep, ReservationStep, Saga, TicketingStep
 
 class TestStep:
     call_count = 0
+    data = None
 
     def make_request(self, endpoint: str, expected_input: dict[str, Any]):
         raise NotImplementedError
@@ -29,56 +30,66 @@ class TestSagaWorkflow(SimpleTestCase):
         self,
     ):
         TestStep.call_count = 0
+        TestStep.data = None
         self.s = Saga(
             [
                 ReservationTestStep(),
                 PaymentTestStep(),
                 TicketingTestStep(),
             ],
-            {},
+            {"passed": "through saga"},
         )
         super().setUp()
 
     def test_happy_path(self):
         def make_request(self, endpoint: str, expected_input: dict[str, Any]):
             TestStep.call_count += 1
-            return {}, True
+            TestStep.data = expected_input
+            return expected_input, True
 
         TestStep.make_request = make_request
         success = self.s.do()
         self.assertTrue(success)
         self.assertEqual(TestStep.call_count, 3)
+        self.assertEqual(TestStep.data, {"passed": "through saga"})
 
     def test_reservation_first_step_fails(self):
         def make_request(self, endpoint: str, expected_input: dict[str, Any]):
             TestStep.call_count += 1
+            TestStep.data = expected_input
             if endpoint == "make-reservation":
-                return {}, False
-            return {}, True
+                return expected_input, False
+            return expected_input, True
 
         TestStep.make_request = make_request
         success = self.s.do()
         self.assertFalse(success)
         self.assertEqual(TestStep.call_count, 1)
+        self.assertEqual(TestStep.data, {"passed": "through saga"})
 
     def test_payment_second_step_fails(self):
         def make_request(self, endpoint: str, expected_input: dict[str, Any]):
             TestStep.call_count += 1
+            TestStep.data = expected_input
+
             if endpoint == "pay":
-                return {}, False
-            return {}, True
+                return expected_input, False
+            return expected_input, True
 
         TestStep.make_request = make_request
         success = self.s.do()
         self.assertFalse(success)
         self.assertEqual(TestStep.call_count, 3)
+        self.assertEqual(TestStep.data, {"passed": "through saga"})
 
     def test_ticket_rendering_third_step_fails(self):
         def make_request(self, endpoint: str, expected_input: dict[str, Any]):
             TestStep.call_count += 1
+            TestStep.data = expected_input
+
             if endpoint == "render-ticket":
-                return {}, False
-            return {}, True
+                return expected_input, False
+            return expected_input, True
 
         TestStep.make_request = make_request
         success = self.s.do()
