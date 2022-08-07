d3f803b40e4a874519b31d3becd47eb69e93f7ba
Paweł Kukliński <paffcio314@gmail.com>
2022-08-07 15:54:23 +0200

    read model wip 2

--- read-model/server.js
+++ read-model/server.js
@@ -1,12 +1,18 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const express = require('express')
-const app = express()
-const port = 3001
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+server.listen(3001, () => {
+  console.log("listening on *:3001");
+});
