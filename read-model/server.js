28e09c041d2220efcec825e06edbffaa27ff24da
Paweł Kukliński <paffcio314@gmail.com>
2022-08-07 20:09:21 +0200

    read model wip 4

--- read-model/server.js
+++ read-model/server.js
@@ -3,6 +3,14 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+global.state = {};
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -14,6 +22,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/update", (req, res) => {
+  console.log(state);
+});
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
