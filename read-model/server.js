17bface44797f9e3cf361360ac0d9b56f511bbfc
Paweł Kukliński <paffcio314@gmail.com>
2022-08-09 23:24:55 +0200

    init set state on read model

--- read-model/server.js
+++ read-model/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const axios = require('axios');
+
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 global.state = {};
 const bodyParser = require("body-parser");
+
+(async () => {
+  try {
+    const response = await axios.get('http://app:80/screenings/76388a51-1fa6-428f-839f-32a1e5091aab/')
+    state = response.data
+    console.log("state set")
+  } catch (error) {
+    console.log(error);
+  }
+})();
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "a^i=_6tz2@@g!-83zlld8x0(q(y7^+6$wh^38f15s7oo^1o$o@"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
