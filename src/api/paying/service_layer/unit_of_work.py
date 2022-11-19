c4951ef6ee39879e66d806ef89953c285e5df283
Paweł Kukliński <paffcio314@gmail.com>
2022-11-19 21:31:45 +0100

    repo and uow for paying service

--- src/api/paying/service_layer/unit_of_work.py
+++ src/api/paying/service_layer/unit_of_work.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 import abc
 from paying.adapters import repository
 from django.db import transaction
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from booking import config
 
 
 class AbstractUnitOfWork(abc.ABC):
@@ -39,3 +42,33 @@ class DjangoUnitOfWork(AbstractUnitOfWork):
 
     def rollback(self):
         transaction.rollback()
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        config.get_postgres_uri("postgres_paying"),
+        isolation_level="REPEATABLE READ",
+        future=True,
+        echo=True,
+    )
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.payments = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def _commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
