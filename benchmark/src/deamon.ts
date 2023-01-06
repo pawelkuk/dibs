cdb973063c5b88a06bfe2ef43c5ecd73f4c92723
Paweł Kukliński <paffcio314@gmail.com>
2023-01-06 20:03:05 +0100

    minor changes

--- benchmark/src/deamon.ts
+++ benchmark/src/deamon.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 type Screening = {
   screening_id: string;
@@ -25,17 +25,19 @@ function sleep(ms: number) {
 async function main() {
   while (true) {
     await sleep(2000);
-    await axios.get<Screening[]>("http://app/screenings").then((res) => {
-      res.data.map((screening: Screening) => {
-        axios
-          .get(`http://app/screenings/${screening.screening_id}/`)
-          .then((res) => {
-            let screeningDetail: ScreeningDetail = res.data;
-            console.log(screeningDetail.movie.title);
-            console.log(screeningDetail.reservations.flatMap((r) => r.seats));
-          });
+    await axios
+      .get<Screening[]>("http://app/screenings")
+      .then((res: AxiosResponse) => {
+        res.data.map((screening: Screening) => {
+          axios
+            .get(`http://app/screenings/${screening.screening_id}/`)
+            .then((res: AxiosResponse) => {
+              let screeningDetail: ScreeningDetail = res.data;
+              console.log(screeningDetail.movie.title);
+              console.log(screeningDetail.reservations.flatMap((r) => r.seats));
+            });
+        });
       });
-    });
   }
 }
 
