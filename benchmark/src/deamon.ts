7fe3b4648de80db8f6bf527c42e16ca24cd5a187
Paweł Kukliński <paffcio314@gmail.com>
2023-01-08 19:47:38 +0100

    minor changes

--- benchmark/src/deamon.ts
+++ benchmark/src/deamon.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosResponse } from "axios";
-
 type Screening = {
   screening_id: string;
   title: string;
@@ -14,7 +13,9 @@ type ScreeningDetail = {
     seats: string[];
     customer_id: string;
   }[];
+  free_seats: string[];
 };
+const API_URL = "http://app/screenings";
 
 function sleep(ms: number) {
   return new Promise((resolve) => {
@@ -24,20 +25,18 @@ function sleep(ms: number) {
 
 async function main() {
   while (true) {
-    await sleep(2000);
-    await axios
-      .get<Screening[]>("http://app/screenings")
-      .then((res: AxiosResponse) => {
-        res.data.map((screening: Screening) => {
-          axios
-            .get(`http://app/screenings/${screening.screening_id}/`)
-            .then((res: AxiosResponse) => {
-              let screeningDetail: ScreeningDetail = res.data;
-              console.log(screeningDetail.movie.title);
-              console.log(screeningDetail.reservations.flatMap((r) => r.seats));
-            });
-        });
+    await sleep(5000);
+    await axios.get<Screening[]>(API_URL).then((res: AxiosResponse) => {
+      res.data.map((screening: Screening) => {
+        axios
+          .get(`${API_URL}/${screening.screening_id}/`)
+          .then((res: AxiosResponse) => {
+            let screeningDetail: ScreeningDetail = res.data;
+            console.log(screeningDetail.free_seats);
+            console.log(screeningDetail.reservations.flatMap((r) => r.seats));
+          });
       });
+    });
   }
 }
 
--- src/api/booking/serializers.py
+++ src/api/booking/serializers.py
@@ -33,6 +33,7 @@ class ScreeningSerializer(serializers.ModelSerializer):
     theatre = TheatreSerializer(read_only=True)
     movie = MovieSerializer(read_only=True)
     reservations = ReservationSerializer(many=True, read_only=True)
+    free_seats = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Screening
@@ -41,7 +42,12 @@ class ScreeningSerializer(serializers.ModelSerializer):
             "theatre",
             "movie",
             "reservations",
+            "free_seats",
         ]
+
+    def get_free_seats(self, obj):
+        screening = obj.to_domain()
+        return [seat.row + str(seat.place) for seat in screening._free_seats]
 
 
 class ScreeningListSerializer(serializers.ModelSerializer):
